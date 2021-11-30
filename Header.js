1942749593d5fd91012bd9f9b8598034161608bc
qaylinholliman <90742428+qaylinholliman@users.noreply.github.com>
2021-11-30 14:31:01 -0600

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-
+import { connect } from "react-redux";
+import { signOutAPI } from "../actions";
 const Header = (props) => {
   return (
     <Container>
@@ -57,12 +58,19 @@ const Header = (props) => {
 
             <User>
               <a>
-                <img src="/images/newuser.svg" alt="" />
-                <span>Me</span>
+                
+                {props.user && props.user.photoURL ? (
+                  <img src={props.user.photoURL} alt="" />
+                ) : (
+                  <img src="/images/user.svg" alt="" />
+                )}
+                <span>
+                  Me
                 <img src="/images/down-icon.svg" alt="" />
+                </span>
               </a>
 
-              <SignOut>
+              <SignOut onClick={() => props.signOut()}>
                 <a>Sign Out</a>
               </SignOut>
             </User>
@@ -251,4 +259,15 @@ const Work = styled(User)`
   border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.userState.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  signOut: () => dispatch(signOutAPI()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
+
