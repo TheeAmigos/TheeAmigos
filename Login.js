4ecab439d40aa8539fc1500ee8d8ec8b910c2dc2
qaylinholliman <90742428+qaylinholliman@users.noreply.github.com>
2021-11-30 14:36:41 -0600

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,8 +1,12 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { signInAPI } from "../actions";
+import { Redirect } from "react-router";
 
 const Login = (props) => {
     return (
     <Container>
+        {props.user && <Redirect to="/home" />}
         <Nav>
             <a href= "/">
                 <img src="/images/JSU1.svg" alt="" />
@@ -18,7 +22,7 @@ const Login = (props) => {
                 <img src="/images/login-hero.svg" alt="" />
             </Hero>
             <Form>
-                    <Google>
+                    <Google onClick={() => props.signIn() }>
                         <img src="/images/google.svg" alt="" />
                         Sign In with Google
                     </Google>
@@ -163,4 +167,17 @@ const Google = styled.button`
         color: rgba(0, 0, 0, 0.75);
     }
 `;
-export default Login;
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.userState.user,
+    };
+};
+
+
+
+const mapDispatchToProps = (dispatch) => ({
+    signIn: () => dispatch(signInAPI()),
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(Login);
